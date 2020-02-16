af10a4dead9b10118f35cf20a9885c57657c0fbe
Connor Sparks <30665220+Atomator@users.noreply.github.com>
2020-02-16 11:01:31 -0700

    created demotask list for testing purposes

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1 class="text-primary">Dashboard</h1>
+    <router-link to="/demotask">
+        <button class="btn btn-dark">Try Demo Task</button>
+    </router-link>
+    <br/>
     <button @click="logout" class="btn btn-primary">Logout</button>
   </div>
 </template>
@@ -22,5 +26,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.btn {
+  margin: 15px;
+}
 
 </style>
---
+++ src/pages/DemoTask.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="text-primary">Demo Task List</h1>
+    <router-link to="/dashboard">
+        <button class="btn btn-dark">Return to Dashboard</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'DemoTask',
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
