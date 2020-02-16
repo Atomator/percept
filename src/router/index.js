dd99edd0ad91ef3479186f6cb039b9d00ba6f1d4
Atomator Stark <30665220+Atomator@users.noreply.github.com>
2020-02-15 20:16:05 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Homepage from '@/pages/Homepage'
-
 import Signup from '@/pages/Signup'
 
-import Login from '@/pages/Login'
 
 Vue.use(Router)
 
@@ -20,10 +18,6 @@ const router = new Router({
       path: '/signup',
       name: 'SignUp',
       component: Signup
-    },
-      path: '/login',
-      name: 'Login',
-      component: Login 
     }
   ]
 })
