b858dd22c964fc1d04420dacd1a0ad9e1d5256c8
Sachthenative2 <38706443+Sachthenative2@users.noreply.github.com>
2020-02-15 18:16:46 -0700

    structure

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Homepage from '@/pages/Homepage'
-
+import Signup from '@/pages/Signup'
 
 Vue.use(Router)
 
@@ -12,6 +12,11 @@ const router = new Router({
         path: '/',
         name: 'Homepage',
         component: Homepage
+    },
+    {
+        path: 'Signup',
+        name: 'Signup',
+        component: Signup
     },
   ]
 })
