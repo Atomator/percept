9b230f1bafe8db8f6533939bbde475cd0837a412
Connor Sparks <ycoloradolego@gmail.com>
2020-02-15 23:01:53 -0700

    got authentication to work

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="text-primary">Dashboard</h1>
+    <button @click="logout" class="btn btn-primary">Logout</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'Dashboard',
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('login')
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
